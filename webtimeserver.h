5251560caafa8d730ed1f46170d3e448eb8d84fe
stasiajaseph <144168269+stasiajaseph@users.noreply.github.com>
2025-04-11 19:08:33 -0700

    Create webtimeserver.h

---
+++ webtimeserver.h
@@ -0,0 +1,11 @@
+// webtimeserver.h - Web Time Server header file
+
+#ifndef WEBTIMESERVER_H
+#define WEBTIMESERVER_H
+
+// Function prototypes
+void startServer();
+void stopServer();
+int getCurrentTime();
+
+#endif // WEBTIMESERVER_H
